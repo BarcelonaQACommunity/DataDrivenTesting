f856782bada0fa644435bd3f34fd4ee34c3ba47c
xavier-cr1 <xaviercasafont@gmail.com>
2017-05-10 18:15:43 +0200

    openxml

---
+++ TestData.OpenXml/ContentManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using PageObject.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData.OpenXml
+{
+    public class ContentManager
+    {
+        public static ICollection<Customer> GetCustomerListFromXls()
+        {
+            ICollection<Customer> customersCollection;
+            try
+            {
+                var excelPath = "excelpath";
+                var excelCustomerDt = OpenXmlConnection.ReadExcelSheet(excelPath, "Customer");
+                customersCollection = OpenXmlConnection.ExcelMappingToCustomer(excelCustomerDt);
+            }
+            catch (Exception)
+            {
+                customersCollection = null;
+            }
+            return customersCollection;
+        }
+
+        public static ICollection<User> GetUserListFromXls()
+        {
+            ICollection<User> usersCollection;
+            try
+            {
+                var excelPath = "excelpath";
+                var excelUserDt = OpenXmlConnection.ReadExcelSheet(excelPath, "User");
+                usersCollection = OpenXmlConnection.ExcelMappingToUser(excelUserDt);
+            }
+            catch (Exception)
+            {
+                usersCollection = null;
+            }
+            return usersCollection;
+        }
+    }
+}
